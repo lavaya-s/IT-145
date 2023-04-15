990852a1c0cbe532400fb30f83416796c2ca63b8
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-04-15 09:37:41 -0400

    Update Driver.java

--- grazioso/Driver.java
+++ grazioso/Driver.java
@@ -74,54 +74,86 @@ public class Driver {
 
 
     // Adds monkeys to a list for testing
-    //Optional for testing
     public static void initializeMonkeyList() {
         Monkey monkey1Monkey = new Monkey(5, 5, 5, "monkey", "banana", "female", "five", "twenty", "1/1/2022", "america", "intermediate", false, "america");
         monkeyList.add(monkey1Monkey);
     }
 
-
-    // Complete the intakeNewDog method
-    // The input validation to check that the dog is not already in the list
-    // is done for you
-    public static void intakeNewDog(Scanner scanner) {
+    // input validation to check that the dog is not already in the list
+    public static void intakeNewDog(Scanner scnr) {
+        System.out.println(" Please enter all input without spaces!!!!");
         System.out.println("What is the dog's name?");
-        String name = scanner.nextLine();
+        String name = scnr.next();
         for(Dog dog: dogList) {
             if(dog.getName().equalsIgnoreCase(name)&&name!=null) {
                 System.out.println("\n\nThis dog is already in our system\n\n");
                 return; //returns to menu
             }
         }
-//TODO:create variable for scnr input and use them in the class instantiation
-        // Add the code to instantiate a new dog and add it to the appropriate list
+        // instantiate a new dog and add it to the appropriate list
         try{
-            Dog dog1Dog = new Dog(name,scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.nextBoolean(),scanner.next());
-            dogList.add(dog1Dog);
-        }catch (Exception inputMismatchException){
+            System.out.println(" Please enter dog breed: ");//prompt the user for input
+            String dogBreed = scnr.next();
+            System.out.println(" Please enter dog gender: ");
+            String dogGender = scnr.next();
+            System.out.println(" Please enter dog age: ");
+            String dogAge = scnr.next();
+            System.out.println(" Please enter dog weight: ");
+            String dogWeight =scnr.next();
+            System.out.println(" Please enter aquisition date: ");
+            String dogAquisitionDate = scnr.next();
+            System.out.println(" Please enter country of aquisition: ");
+            String dogAquisitionCountry = scnr.next();
+            System.out.println(" Please enter training status of dog: ");
+            String dogTrainingStatus = scnr.next();
+            boolean dogReserve = false;
+            System.out.println(" Please enter dog country: ");
+            String dogCountry = scnr.next();
+            Dog dog1Dog = new Dog(name,dogBreed,dogGender,dogAge,dogWeight,dogAquisitionDate,dogAquisitionCountry,dogTrainingStatus,dogReserve,dogCountry);
+            dogList.add(dog1Dog);//add the new dog to the list
+        }catch (Exception inputMismatchException){//executes if the user inputs an invalid entry
             System.out.println("Invalid input, Please try again");
             return;
         }
         return;
     }
 
-
-        // Complete intakeNewMonkey
 	//Instantiate and add the new monkey to the appropriate list
-        // For the project submission you must also  validate the input
-	// to make sure the monkey doesn't already exist and the species type is allowed
-    public static void intakeNewMonkey(Scanner scanner) {
-        System.out.println("The method intakeNewMonkey needs to be implemented");
+    public static void intakeNewMonkey(Scanner scnr) {
+        System.out.println("Please enter all entres without spaces!!!!!");
         System.out.println("What is the monkey's name?");
-        String name = scanner.nextLine();
-        for(Monkey monkey: monkeyList) {
+        String name = scnr.next();
+        for(Monkey monkey: monkeyList) {//iterate over the monkeys in the list
             if(monkey.getName().equalsIgnoreCase(name) && name != null) {//check to make sure the name is not empty and not already taken. 
                 System.out.println("\n\nThis monkey is already in our system\n\n");
                 return; //returns to menu
             }
         }
         try{
-            Monkey Monkey1 = new Monkey(scanner.nextDouble(),scanner.nextDouble(),scanner.nextDouble(),scanner.next(),name,scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.next(),scanner.nextBoolean(),scanner.next());
+            System.out.println(" Please enter length of the monkeys tail: ");
+            double tailLength = scnr.nextDouble();
+            System.out.println(" Please enter monkey height: ");
+            double height = scnr.nextDouble();
+            System.out.println(" Please enter monkey body length: ");
+            double bodyLength = scnr.nextDouble();
+            System.out.println(" Please enter monkey species: ");
+            String species = scnr.next();
+            System.out.println(" Please enter monkey gender: ");
+            String gender = scnr.next();
+            System.out.println(" Please enter monkey age: ");
+            String age = scnr.next();
+            System.out.println(" Please enter monkey weight: ");
+            String weight =scnr.next();
+            System.out.println(" Please enter aquisition date: ");
+            String acqDate = scnr.next();
+            System.out.println(" Please enter country of aquisition: ");
+            String acqCountry = scnr.next();
+            System.out.println(" Please enter training status of monkey: ");
+            String trainingStatus = scnr.next();
+            boolean reserved = false;
+            System.out.println(" Please enter monkey country: ");
+            String inServiceCountry = scnr.next();
+            Monkey Monkey1 = new Monkey(tailLength,height,bodyLength,species,name,gender,age,weight,acqDate,acqCountry,trainingStatus,reserved,inServiceCountry);
             monkeyList.add(Monkey1);
         }catch (Exception inputMismatchException){
             System.out.println("Invalid input, Please try again");
@@ -132,21 +164,70 @@ public class Driver {
 
         // Complete reserveAnimal
         // You will need to find the animal by animal type and in service country
-        public static void reserveAnimal(Scanner scanner) {
-            System.out.println("The method reserveAnimal needs to be implemented");
-        
+        public static void reserveAnimal(Scanner scnr) {
+            System.out.println("Would you like to reserve a dog or monkey?");
+            String animal = scnr.next();//check to see which animal is being reserved
+            boolean reserved = true;
+            if (animal.equalsIgnoreCase("monkey")){
+                System.out.print("Please enter the monkeys name: ");
+                String name = scnr.next();
+                for(Monkey monkey: monkeyList) {
+                    if(monkey.getName().equalsIgnoreCase(name) && name != null) {//check to make sure the name is not empty and not already taken. 
+                        (monkey).setReserved(reserved);//reserves the animal
+                        System.out.println("Thank you for reserving this animal");
+                        return; //returns to menu
+                    }
+                }
+                System.out.println("This monkey is not in our system");
+                return;
+            }
+            if (animal.equalsIgnoreCase("dog")){
+                System.out.print("Please enter the dogs name: ");
+                String name = scnr.next();
+                for(Dog dog: dogList) {
+                    if(dog.getName().equalsIgnoreCase(name) && name != null) {//check to make sure the name is not empty and not already taken. 
+                        (dog).setReserved(reserved);
+                        System.out.println("Thank you for reserving this animal");
+                        return; //returns to menu
+                    }
+                }
+                System.out.println("This dog is not in our system");
+                return;
+            }
+            System.out.println("You must enter (dog/monkey)");
         }
         public static void printAnimals(String switchCase) {
             //System.out.println("The method printAnimals needs to be implemented");
-            switch (switchCase){
+            switch (switchCase){//continues from the previous switch cases based on the user input
                 case "4":
-                    System.out.println(dogList);
+                    for(Dog dog: dogList){
+                        if (dog.getReserved()!=true){
+                            String name = dog.getName();
+                            String location = dog.getInServiceLocation();
+                            System.out.format(name);
+                            System.out.println(": "+location);
+                        }
+                    }
                     break;
                 case "5":
-                    System.out.println(monkeyList);
+                    for(Monkey monkey: monkeyList){
+                        if (monkey.getReserved()!=true){
+                            System.out.format(monkey.getName());
+                            System.out.println(": "+monkey.getInServiceLocation());
+                        }
+                    }
                     break;
                 case "6":
-                    System.out.println("this option needs to be implemented");
+                    for(Monkey monkey: monkeyList){
+                        if (monkey.getReserved()==true){
+                            System.out.println(monkey.getName());
+                        }
+                    }
+                    for(Dog dog: dogList){
+                        if (dog.getReserved()==true){
+                            System.out.println(dog.getName());
+                        }
+                    }
                     break;
             }
             return;
