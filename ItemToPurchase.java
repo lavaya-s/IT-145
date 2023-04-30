a475913015a8a996ceec3b737f3394bc35f2860c
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-04-30 16:59:24 -0400

    remove whitespace

--- ItemToPurchase.java
+++ ItemToPurchase.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
-
 public class ItemToPurchase extends ShoppingCartPrinter{
    //Private fields - itemName, itemPrice, and itemQuanity
-
-   
    private static void ItemToPurchase(){
       String itemName = "none";
       int itemPrice = 0;
@@ -14,35 +11,27 @@ public class ItemToPurchase extends ShoppingCartPrinter{
       Scanner scnr = new Scanner(System.in);
       System.out.println("Enter the item name:");
       String itemName=productName;
-      
       scnr.close();
-      
-      
    }
    public String getName(String itemName){
-      
       return itemName;
    }
    public void setPrice(int productPrice){
       System.out.println("Enter the item price:");
       Scanner scnr = new Scanner(System.in);
       int itemPrice=productPrice;
-      
       scnr.close();
    }
    public int getPrice(int itemPrice){
-      
       return itemPrice;
    }
    public void setQuantity(int productQuantity){
       System.out.println("Enter the item quantity:");
       Scanner scnr = new Scanner(System.in);
       int itemQuantity=productQuantity;
-      
       scnr.close();
    }
    public int getQuantity(int itemQuantity){
-      
       return itemQuantity;
    }
    //print item to purchase
